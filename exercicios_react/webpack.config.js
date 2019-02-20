c5394dae84983de7ff4f65b8969d3de6591ab2ea
Thais <thaysremonti@gmail.com>
2019-02-20 20:37:30 -0300

    curso react

---
+++ exercicios_react/webpack.config.js
@@ -0,0 +1,26 @@
+const webpack = require('webpack')
+
+module.exports = {
+    entry: './ex/index.jsx',
+    output: {
+        path: __dirname + '/public',
+        filename: './bundle.js'
+    },
+    devServer: {
+        port: 8081,
+        contentBase: './public',
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+    module: {
+        loaders: [{
+            test: /.jsx?$/,
+            loader: 'babel-loader',
+            excude: /node_modules/,
+            query: {
+                presets: ['es2015', 'react']
+            }
+        }]
+    }
+}
